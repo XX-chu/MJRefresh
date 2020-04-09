74d16e9b5c01f7634827afc674d4ea10a74ba6f7
楚晓博 <99239476@qq.com>
2020-04-09 10:42:40 +0800

    4.9

--- MJRefresh/NSBundle+MJRefresh.m
+++ MJRefresh/NSBundle+MJRefresh.m
@@ -15,9 +15,25 @@
 {
     static NSBundle *refreshBundle = nil;
     if (refreshBundle == nil) {
+
+        NSUserDefaults *def = [NSUserDefaults standardUserDefaults];
+        NSString *currLanguage = [def valueForKey:@"LocalLanguageKey"];
+        if(!currLanguage){
+            NSArray *preferredLanguages = [NSLocale preferredLanguages];
+            currLanguage = preferredLanguages[0];
+            if ([currLanguage hasPrefix:@"ug"]) {
+                currLanguage = @"ug-CN";
+            }else if ([currLanguage hasPrefix:@"zh"]) {
+                currLanguage = @"zh-Hans";
+            }else currLanguage = @"zh-Hans";
+            [def setValue:currLanguage forKey:@"LocalLanguageKey"];
+            [def synchronize];
+        }
         // 这里不使用mainBundle是为了适配pod 1.x和0.x
-        refreshBundle = [NSBundle bundleWithPath:[[NSBundle bundleForClass:[MJRefreshComponent class]] pathForResource:@"MJRefresh" ofType:@"bundle"]];
+        NSString *path = [[NSBundle mainBundle] pathForResource:currLanguage ofType:@"lproj"];
+        refreshBundle = [NSBundle bundleWithPath:path];//生成bundle
     }
+    
     return refreshBundle;
 }
 
@@ -37,37 +53,25 @@
 
 + (NSString *)mj_localizedStringForKey:(NSString *)key value:(NSString *)value
 {
-    static NSBundle *bundle = nil;
-    NSString *language = MJRefreshConfig.defaultConfig.languageCode;
-    // 如果配置中没有配置语言
-    if (!language) {
-        // （iOS获取的语言字符串比较不稳定）目前框架只处理en、zh-Hans、zh-Hant三种情况，其他按照系统默认处理
-        language = [NSLocale preferredLanguages].firstObject;
+    NSUserDefaults *def = [NSUserDefaults standardUserDefaults];
+    NSString *currLanguage = [def valueForKey:@"LocalLanguageKey"];
+    if(!currLanguage){
+        NSArray *preferredLanguages = [NSLocale preferredLanguages];
+        currLanguage = preferredLanguages[0];
+        if ([currLanguage hasPrefix:@"ug"]) {
+            currLanguage = @"ug-CN";
+        }else if ([currLanguage hasPrefix:@"zh"]) {
+            currLanguage = @"zh-Hans";
+        }else currLanguage = @"zh-Hans";
+        [def setValue:currLanguage forKey:@"LocalLanguageKey"];
+        [def synchronize];
     }
     
-    if ([language hasPrefix:@"en"]) {
-        language = @"en";
-    } else if ([language hasPrefix:@"zh"]) {
-        if ([language rangeOfString:@"Hans"].location != NSNotFound) {
-            language = @"zh-Hans"; // 简体中文
-        } else { // zh-Hant\zh-HK\zh-TW
-            language = @"zh-Hant"; // 繁體中文
-        }
-    } else if ([language hasPrefix:@"ko"]) {
-        language = @"ko";
-    } else if ([language hasPrefix:@"ru"]) {
-        language = @"ru";
-    } else if ([language hasPrefix:@"uk"]) {
-        language = @"uk";
-    }else if ([language hasPrefix:@"uy"]) {
-        language = @"uy";
-    } else {
-        language = @"en";
-    }
+    //获取文件路径
+    NSString *path = [[NSBundle mainBundle] pathForResource:currLanguage ofType:@"lproj"];
+    NSBundle *bundle = [NSBundle bundleWithPath:path];//生成bundle
+    NSString *revalue = [bundle localizedStringForKey:key value:nil table:@"Localizable"];
     
-    // 从MJRefresh.bundle中查找资源
-    bundle = [NSBundle bundleWithPath:[[NSBundle mj_refreshBundle] pathForResource:language ofType:@"lproj"]];
-    value = [bundle localizedStringForKey:key value:value table:nil];
-    return [[NSBundle mainBundle] localizedStringForKey:key value:value table:nil];
+    return revalue;
 }
 @end
